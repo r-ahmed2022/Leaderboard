f5b71648860dc2fb92c2d3108feb4d7ed529bda2
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-07-28 21:45:01 +0530

    created interface for API hit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 // eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
+import { refreshApiData, addEntry } from './modules/api.js';
+
+const playerBox = document.querySelector('.list');
+const refreshBtn = document.getElementById('refresh');
+const form = document.getElementById('score-form');
+const name = document.getElementById('name');
+const score = document.getElementById('score');
+let players;
+
+window.addEventListener('load', () => {
+  refreshApiData(players, playerBox);
+});
+
+refreshBtn.addEventListener('click', () => {
+  refreshApiData(players, playerBox);
+});
---
+++ src/modules/api.js
@@ -0,0 +1,41 @@
+/* eslint-disable linebreak-style */
+import showRanks from './showRanks.js';
+
+export const refreshApiData = (players, playerBox) => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/a9b9cdf78dfb964d4ad19ba5e0292907;o=1/scores')
+    .then((response) => response.json())
+    .then((json) => {
+      players = json.result;
+      showRanks(players, playerBox);
+    });
+};
+
+export const createGame = () => {
+  const obj = {
+    name: 'FIFA',
+  };
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    body: JSON.stringify(obj),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => json);
+};
+
+export const addEntry = async (name, score) => {
+  const obj = {
+    user: name.value,
+    score: score.value,
+  };
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/a9b9cdf78dfb964d4ad19ba5e0292907;o=1/scores', {
+    method: 'POST',
+    body: JSON.stringify(obj),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return response.result;
+};
